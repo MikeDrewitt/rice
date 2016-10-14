2bc9cab7f91e571e42cc6155a1a84f7fda9ccdda
Michael Drewitt <mikedrewitt@gmail.com>
2016-10-13 21:26:24 -0400

    Clean up!

--- .scripts/keyBoardLED_Up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-brightness=$(cat "/sys/class/leds/smc::kbd_backlight/brightness")
-a=$((brightness+25))
-
-if [ "$brightness" -lt 250 ]
-then
-echo $a >> /sys/class/leds/smc\:\:kbd_backlight/brightness
-fi
