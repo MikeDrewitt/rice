f4d2e233621fd95f8ad46607cbf023ecfc1d430d
Michael Drewitt <mikedrewitt@gmail.com>
2016-11-01 12:42:28 -0400

    cleanup scritps and make blur faster

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,3 +1,9 @@
-i3lock -nu -i "/home/michael/.scripts/lockScreen.png"
+#!/bin/bash
+
+scrot /home/michael/.scripts/lock_screen.png
+
+ffmpeg -loglevel quiet -y -i ~/.scripts/lock_screen.png -vf "gblur=sigma=10" ~/.scripts/lock_screen.png
+
+i3lock -nu -i "/home/michael/.scripts/lock_screen.png"
 
 
