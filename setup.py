902df84fd184ec809c916ac0bb5a9ebd76f94927
Michael Drewitt <mikedrewitt@gmail.com>
2016-12-06 14:49:06 -0500

    added python setup script rather than bash

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+'''
+This program will read from the install_list.txt
+
+It installs everything there so long as the section is called 
+as a parameter in at the time of calling this script.
+
+ex: python setup.py programs rice desktop
+'''
+
+import os 
+import sys
+
+INSTALL_LIST = 'install_list.txt'
+
+aur_packages = os.popen('pacman -Q').read()
+aur_packages = aur_packages.split()
+
+# kinda gross because the aur installed packages list also has 
+# the versions but for our purposes it won't matter, and goes unsceen anyways
+
+with open(INSTALL_LIST, 'r') as install_list:
+    for install in install_list:
+        if install[0] == '#':
+            flag = install[1:].strip() 
+        else:
+            try:
+                program = install.split()[0]
+                #print(program)
+                if not program in aur_packages and flag in sys.argv:
+                    os.system('yaourt -S --noconfirm ' + program)
+                    #print(program)
+            except IndexError:
+                pass
+     
