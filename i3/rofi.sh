b3e66dfa6bfb1cf91b9ab8ec6ecbbaeb6532fdc2
Michael Drewitt <mikedrewitt@gmail.com>
2017-01-16 12:14:24 -0500

    rofi color swap

--- i3/rofi.sh
+++ i3/rofi.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-rofi -modi "run,drun,ssh" -show drun -width 2560 -padding 600 -lines 9 -eh 2 -location 0 -opacity "85" -bw 0 -hide-scrollbar -color-enabled -color-window "#202020","#202020" -color-normal "#202020","#FFFFFF","#202020","#ff7220","#303030"
+rofi -modi "run,drun,ssh" 	\
+	 -show drun 			\
+	 -width 2560 			\
+	 -padding 600 			\
+	 -lines 15 				\
+	 -eh 2 					\
+	 -location 0 			\
+	 -opacity "10" 			\
+	 -bw 0 					\
+	 -hide-scrollbar 		\
+	 -color-enabled 		\
+	 -color-window "#0B3954","#0B3954" \
+	 -color-normal "#0B3954","#BFD7EA","#0B3954","#FF5A5F","#CC303030"
 
 #-width 1280 -padding 100 
